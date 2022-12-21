80fba322f68002ce66d50cba14f8bc6a03ea8e0e
Fatalis <connman2400@gmail.com>
2022-12-20 19:00:27 -0600

    added request model

---
+++ models/Requests.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+const { init } = require('./Parents');
+
+class Request extends Model{}
+
+Request.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+          },
+          date:{
+            type: DataTypes.DATE,
+            allowNull:false
+          },
+          comment:{
+            type: DataTypes.STRING,
+            allowNull: true
+          },
+          accepted:{
+            type:DataTypes.BOOLEAN
+          },
+          parents_id:{
+            type: DataTypes.INTEGER,
+            references:{
+              model:'parents',
+              key:'id'
+            }
+          }
+          
+
+          
+          
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'children'
+        }
+);
+
+module.exports = Request;
