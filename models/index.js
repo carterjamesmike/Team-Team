86dcbe41c5fe7ca1efca2aa88c1da239cb069ef1
Fatalis <connman2400@gmail.com>
2022-12-22 19:08:05 -0600

    model relations

--- models/index.js
+++ models/index.js
@@ -1,5 +1,36 @@
 const Parents = require('./Parents');
 const Accept = require('./Accept');
 const Children = require('./Children');
-const Reuests = require('./Requests');
-module.exports = { Parents, Accept, Children, Reuests }
+const Requests = require('./Requests');
+
+Parents.hasMany(Children,{
+    foreignKey:'parent_id'
+});
+
+Parents.hasMany(Requests,{
+    foreignKey:'parent_id',
+    onDelete:"cascade"
+});
+
+Parents.hasMany(Accept,{
+    foreignKey:"parent_id",
+    onDelete:"cascade"
+});
+
+Children.belongsTo(Parents,{
+    foreignKey:"parent_id",
+    onDelete:"cascade"
+});
+
+Requests.belongsTo(Parents,{
+    foreignKey:"parent_id",
+    onDelete:"cascade"
+});
+
+Requests.belongsTo(Children,{
+    foreignKey:"children_id",
+    onDelete:"cascade"
+});
+
+module.exports = { Parents, Accept, Children, Requests }
+
