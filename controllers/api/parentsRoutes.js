b9311a8f891f778821a3d18ca63ff9343d8539c7
James Carter <carterjamesmike@gmail.com>
2022-12-22 20:22:15 -0600

    first route with test handlebars complete

--- controllers/api/parentsRoutes.js
+++ controllers/api/parentsRoutes.js
@@ -1,12 +1,23 @@
 const router = require('express').Router();
-const { Parents } = require('../../models');
+//const { Parents } = require('../../models');
 
 
-router.get('/', (req, res) => {
-    Parents.findAll().then((parentData) => {
-        res.json(parentData)
-    });
-});
+// router.get('/', async (req, res) => {
+//     try {
+//         const parentData = await Parents.findAll();
+    
+//         // Serialize data so the template can read it
+//         const parents = parentData.map(([parent]) => parent.get({ plain: true }));
+    
+//         // Pass serialized data and session flag into template
+//         res.render('test', { 
+//           parents, 
+//         });
+//       } catch (err) {
+//         res.status(500).json(err);
+//       }
+
+// });
 
 
 module.exports = router;
