511e96c5640604abf891dd8fdfe266795a13a07d
Fatalis <connman2400@gmail.com>
2022-12-22 21:26:42 -0600

    request route added

---
+++ controllers/api/requestRoutes.js
@@ -0,0 +1,55 @@
+const router = require('express').Router();
+const { Request } = require('../../models');
+
+
+router.get('/', (req, res) => {
+    Request.findAll().then((requestData) => {
+        res.json(requestData)
+    });
+});
+
+
+router.post('/',async(req,res)=>{
+    try{
+    const dbRequestData =await Request.create({
+        title: req.body.title,
+        date: req.body.date,
+        comment: req.body.comment,
+        accepted:false,
+        parents_id: req.body.parents_id
+    });
+    res.status(200).json(dbRequestData);
+}catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.delete('/:id',async(req,res)=>{
+    try{
+    const dbRequestData =await Request.destroy({
+        where:{
+            id: req.params.id
+        }
+    });
+    res.status(200).json(dbRequestData);
+}catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id',async(req,res)=>{
+    try{
+    const dbRequestData =await Request.update(req.body,{
+        where:{
+            id: req.params.id
+        }
+    });
+    res.status(200).json(dbRequestData);
+}catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+})
+module.exports = router;
