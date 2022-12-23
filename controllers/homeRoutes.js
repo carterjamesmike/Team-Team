7c2edf19b5348ebba16e849c3df4ca8d3c160977
James Carter <carterjamesmike@gmail.com>
2022-12-22 21:29:42 -0600

    Get route templates complete

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { Accept, Children, Parents, Requests } = require('../models');
 const withAuth = require('../utils/auth');
 
+//Display all parents (general GET route)
 router.get('/', async (req, res) => {
     try {
         const parentData = await Parents.findAll();
@@ -15,6 +16,36 @@ router.get('/', async (req, res) => {
 
     } catch (err) { res.status(500).json(err) }
 });
+
+//Display individual parent by ID (general GET by ID route)
+router.get('/parent/:id', async (req, res) => {
+    try {
+      const parentData = await Parents.findByPk(req.params.id);
+  
+      const parent = parentData.get({ plain: true });
+  
+      res.render('parent', {
+        parent,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+        console.log(err)
+      res.status(500).json(err);
+    }
+  });
+
+//Renders add a request page (general redirect route)
+router.get('/request', checkAuth, async (req, res) => {
+    try {
+        res.render('request', { 
+        logged_in: req.session.logged_in 
+    });
+    } catch (err) { 
+        console.log(err)
+        res.status(500).json(err) }
+});
+
+
 
 
 module.exports = router;
