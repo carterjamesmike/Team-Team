1482587a259e5a4cd5d3994ea629c07bffa16190
James Carter <carterjamesmike@gmail.com>
2023-01-03 20:01:23 -0600

    Login handlebars in progress, commit to merge with Conner's parents controllers

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -35,7 +35,7 @@ router.get('/parent/:id', async (req, res) => {
   });
 
 //Renders add a request page (general redirect route)
-router.get('/request', checkAuth, async (req, res) => {
+router.get('/request', withAuth, async (req, res) => {
     try {
         res.render('request', { 
         logged_in: req.session.logged_in 
@@ -45,7 +45,26 @@ router.get('/request', checkAuth, async (req, res) => {
         res.status(500).json(err) }
 });
 
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/parent');
+    return;
+  }
+  
 
+  res.render('login');
+});
+
+router.get('/parent', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  // if (req.session.logged_in) {
+  //   res.redirect('/parent');
+  //   return;
+  // }
+
+  res.render('parent');
+});
 
 
 module.exports = router;
